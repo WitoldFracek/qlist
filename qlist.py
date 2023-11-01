f9d8ec7ad73a8199256685ef94dc927225660ef4
Witold Frącek <witek.fracek@gmail.com>
2023-11-01 20:11:27 +0100

    Some doc comments and minor typing adjustements

--- qlist.py
+++ qlist.py
@@ -5,29 +5,43 @@ K = TypeVar('K')
 SupportsLessThan = TypeVar("SupportsLessThan")
 
 
-class QList(Generic[T]):
-    pass
-
-
 class Lazy(Generic[T]):
-    pass
+    """
+    Object representing lazy evaluation of called methods.
 
+    Warning:
+    -------
+    -------
 
-class Lazy(Generic[T]):
-    def __init__(self, gen: Iterator[T]):
+    Calling any method consumes the current Lazy object. Using the same object
+    again may cause errors due to the draining of the generator.
+
+    Example:
+    -------
+    -------
+
+    .. sourcecode::
+
+        qlist = QList([1, 2, 3, 4])
+        filtered = qlist.filter(lambda x: x < 3)
+        mapped_str = filtered.map(str)
+        mapped_float = filtered.map(float)
+        print(mapped_float.qlist())    # prints [1.0, 2.0]
+        print(mapped_str.qlist())    # prints []
+    """
+    def __init__(self, gen: Iterable[T]):
         self.gen = gen
 
     def list(self) -> list[T]:
         return [elem for elem in self.gen]
 
-    def qlist(self) -> QList[T]:
+    def qlist(self):
         return QList(elem for elem in self.gen)
 
-    def filter(self, pred: Callable[[T], bool]) -> Lazy[T]:
+    def filter(self, pred: Callable[[T], bool]):
         """
         Returns a Lazy object containing all values from this Lazy object for which
         the predicate holds true.
-
         :param pred: function from T to bool
         """
         def inner():
@@ -42,7 +56,7 @@ class Lazy(Generic[T]):
                 yield mapper(elem)
         return Lazy(inner())
 
-    def fold(self, operation: Callable[[K, T], K], init: K):
+    def fold(self, operation: Callable[[K, T], K], init: K) -> K:
         acc = init
         for elem in self.gen:
             acc = operation(acc, elem)
@@ -52,17 +66,17 @@ class Lazy(Generic[T]):
         for elem in self.gen:
             action(elem)
 
-    def flatmap(self, mapper: Callable[[T], Lazy[K]]):
+    def flatmap(self, mapper: Callable[[T], Iterable[K]]):
         def inner():
             for elem in self.gen:
                 yield from mapper(elem)
         return Lazy(inner())
 
-    def collect(self) -> QList[T]:
+    def collect(self):
         return QList(x for x in self.gen)
 
     def __iter__(self):
-        return self.gen
+        return iter(self.gen)
 
 
 class QList(list, Generic[T]):
@@ -89,19 +103,19 @@ class QList(list, Generic[T]):
         for elem in self:
             action(elem)
 
-    def fold(self, operation: Callable[[K, T], K], init: K):
+    def fold(self, operation: Callable[[K, T], K], init: K) -> K:
         acc = init
         for elem in self:
             acc = operation(acc, elem)
         return acc
 
-    def fold_right(self, operation: Callable[[K, T], K], init: K):
+    def fold_right(self, operation: Callable[[K, T], K], init: K) -> K:
         acc = init
         for elem in self[::-1]:
             acc = operation(acc, elem)
         return acc
 
-    def len(self):
+    def len(self) -> int:
         """
         Returns the len of the QList
 
@@ -109,7 +123,7 @@ class QList(list, Generic[T]):
         """
         return len(self)
 
-    def flatmap(self, mapper: Callable[[T], QList[K]]) -> Lazy[K]:
+    def flatmap(self, mapper: Callable[[T], Iterable[K]]) -> Lazy[K]:
         def inner():
             for elem in self:
                 yield from mapper(elem)
@@ -131,21 +145,12 @@ class QList(list, Generic[T]):
 
 
 if __name__ == '__main__':
-    def triple(arg):
-        return QList([arg, arg, arg])
+    qlist = QList([1, 2, 3, 4])
+    filtered = qlist.filter(lambda x: x < 3)
+    mapped_str = filtered.map(str)
+    mapped_float = filtered.map(float)
+    print(mapped_float.collect())
+    print(mapped_str.collect())
 
-    def lazy_triple(arg):
-        return Lazy(iter(QList([arg, arg, arg])))
-
-    def reductor(prev: QList, cur):
-        if not prev:
-            prev.append(cur)
-            return prev
-        if prev[-1] != cur:
-            prev.append(cur)
-        return prev
-
-    ql = QList(x for x in range(10)).flatmap(triple).flatmap(lazy_triple).fold(reductor, QList()).sort(reverse=True)
-    print(ql)
 
 
