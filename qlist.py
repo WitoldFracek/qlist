39ce8438a155c0d527d8d6acc30b4ee2297a129b
Witold Frącek <witek.fracek@gmail.com>
2023-11-01 19:32:38 +0100

    better typing?

--- qlist.py
+++ qlist.py
@@ -2,19 +2,34 @@ from typing import TypeVar, Generic, Iterable, Iterator, Callable, Generator
 
 T = TypeVar('T')
 K = TypeVar('K')
+SupportsLessThan = TypeVar("SupportsLessThan")
+
+
+class QList(Generic[T]):
+    pass
 
 
 class Lazy(Generic[T]):
-    def __init__(self, gen: Generator[T, None, None] | Iterator[T]):
+    pass
+
+
+class Lazy(Generic[T]):
+    def __init__(self, gen: Iterator[T]):
         self.gen = gen
 
     def list(self) -> list[T]:
         return [elem for elem in self.gen]
 
-    def qlist(self):
+    def qlist(self) -> QList[T]:
         return QList(elem for elem in self.gen)
 
-    def filter(self, pred: Callable[[T], bool]):
+    def filter(self, pred: Callable[[T], bool]) -> Lazy[T]:
+        """
+        Returns a Lazy object containing all values from this Lazy object for which
+        the predicate holds true.
+
+        :param pred: function from T to bool
+        """
         def inner():
             for elem in self.gen:
                 if pred(elem):
@@ -37,18 +52,27 @@ class Lazy(Generic[T]):
         for elem in self.gen:
             action(elem)
 
-    def flatmap(self, mapper: Callable[[T], Iterable[K]]):
+    def flatmap(self, mapper: Callable[[T], Lazy[K]]):
         def inner():
             for elem in self.gen:
                 yield from mapper(elem)
         return Lazy(inner())
 
-    def collect(self):
+    def collect(self) -> QList[T]:
         return QList(x for x in self.gen)
+
+    def __iter__(self):
+        return self.gen
 
 
 class QList(list, Generic[T]):
     def filter(self, pred: Callable[[T], bool]) -> Lazy[T]:
+        """
+        Returns a Lazy object containing all values from the QList for which
+        the predicate holds true.
+
+        :param pred: function from T to bool
+        """
         def inner():
             for elem in self:
                 if pred(elem):
@@ -78,18 +102,40 @@ class QList(list, Generic[T]):
         return acc
 
     def len(self):
+        """
+        Returns the len of the QList
+
+        (time complexity: O(1))
+        """
         return len(self)
 
-    def flatmap(self, mapper: Callable[[T], Iterable[K]]) -> Lazy[K]:
+    def flatmap(self, mapper: Callable[[T], QList[K]]) -> Lazy[K]:
         def inner():
             for elem in self:
                 yield from mapper(elem)
         return Lazy(inner())
 
+    def sort(self, key: Callable[[T], SupportsLessThan] = lambda x: x, reverse=False):
+        """
+        Returns a new QList containing all items from the original list in ascending order.
+
+        A custom key function can be supplied to customize the sort order, and the reverse
+        flag can be set to request the result in descending order.
+
+        !! Overrides the default `sort` method in the `list` object !!
+        :param key:
+        :param reverse:
+        :return: sorted QList
+        """
+        return QList(sorted(self, key=key, reverse=reverse))
+
 
 if __name__ == '__main__':
     def triple(arg):
         return QList([arg, arg, arg])
+
+    def lazy_triple(arg):
+        return Lazy(iter(QList([arg, arg, arg])))
 
     def reductor(prev: QList, cur):
         if not prev:
@@ -99,7 +145,7 @@ if __name__ == '__main__':
             prev.append(cur)
         return prev
 
-    ql = QList(x for x in range(10)).flatmap(triple).flatmap(triple).fold(reductor, QList())
+    ql = QList(x for x in range(10)).flatmap(triple).flatmap(lazy_triple).fold(reductor, QList()).sort(reverse=True)
     print(ql)
 
 
